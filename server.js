c1d4d8537ca26e3709aa657b332bd0e00bd85146
nalo-droid <69765090+nalo-droid@users.noreply.github.com>
2025-01-01 17:57:56 +0300

    new dependecy and server .env

--- server.js
+++ server.js
@@ -35,7 +35,10 @@ app.use((err, req, res, next) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB:', err));
 
