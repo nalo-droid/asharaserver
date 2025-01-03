21d4f2e71119485790030dce9d54a567e06598d0
nalo-droid <69765090+nalo-droid@users.noreply.github.com>
2025-01-03 12:01:57 +0300

    some posts

--- server.js
+++ server.js
@@ -6,17 +6,8 @@ require('dotenv').config();
 
 const app = express();
 
-// Updated CORS configuration
-app.use(cors({
-  origin: [
-    'https://asharaclient.vercel.app',
-    //'http://localhost:5173', // For local development
-    //'http://localhost:3000'  // Alternative local port
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -44,10 +35,7 @@ app.use((err, req, res, next) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB:', err));
 
